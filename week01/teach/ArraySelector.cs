215f370311a163f78d539b6092792956978043a3
abrenzink <a.brenzink@gmail.com>
2024-02-25 00:29:40 -0300

    Problem 2: complicated problem

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -6,11 +6,28 @@ public static class ArraySelector
         var l2 = new[] { 2, 4, 6, 8, 10};
         var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
         var intResult = ListSelector(l1, l2, select);
+        // var intResult = select;
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return new int[0];
+        List<int> selectedList = new List<int>();
+
+        //manage element array index to be stored
+        int count = 0;
+
+        foreach(int selector in select){
+            if(selector == 1){
+               if (count < list1.Length) selectedList.Add(list1[count]);
+            }
+            else if(selector == 2){
+               if (count < list2.Length) selectedList.Add(list2[count]);
+            }
+            count += 1;
+        }
+
+        int[] selectedArray = selectedList.ToArray();
+        return selectedArray;
     }
 }
