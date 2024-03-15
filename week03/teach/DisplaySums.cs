04f6bae63170a5655582c0dd7e46902a79599237
abrenzink <a.brenzink@gmail.com>
2024-03-15 01:16:36 -0300

    week 03 teach activities

--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -29,5 +29,14 @@
     /// <param name="numbers">array of integers</param>
     private static void DisplaySumPairs(int[] numbers) {
         // TODO Problem 2 - This should print pairs of numbers in the given array
+
+        HashSet<int> checkedNum = new HashSet<int>();
+
+        foreach(int num in numbers){
+            if(checkedNum.Contains(10 - num)){
+                 Console.WriteLine($"{num} {10 - num}");
+            }
+            checkedNum.Add(num);
+        }
     }
 }
